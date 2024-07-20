6cf535c07aa9927eda9bcfd1398d3888ad1bfe3e
prathmeshthakare <prathmeshthakare76@gmail.com>
2024-07-20 15:10:59 +0530

    add text summary

--- src/component/TextArea.jsx
+++ src/component/TextArea.jsx
@@ -1,31 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 function TextArea() {
 
-
-    // convert to uppercase 
-    const toUpper = ()=>{
+    const toUpper = () => {
         outText(inText.toUpperCase());
     }
 
-    // for converting to lowercase 
-    const toLower = ()=>{
+    const toLower = () => {
         outText(inText.toLowerCase());
     }
 
     const [inText, outText] = useState("");
-
     return (
         <>
             <div className=" container my-5 ">
-                <h1 className="my-3">Text Convertor </h1>
-                <p className="my-2">Change your text case .....</p>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Enter your text here." value={inText} onChange={(e)=>{outText(e.target.value)}} ></textarea>
-                <button className="btn btn-primary mt-4" onClick={toUpper} >UpperCase</button>
-                <button className="btn btn-primary mt-4 ms-4" onClick={toLower} >LowerCase </button>
+                <h2 className="my-3">Enter the text to analyze  </h2>
+                <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Enter your text here." value={inText} onChange={(e) => { outText(e.target.value) }} ></textarea>
+
+                <div className="container text-center ">
+                    <button className="btn btn-success mt-4" onClick={toUpper} >UpperCase</button>
+                    <button className="btn btn-danger  mt-4 ms-4" onClick={toLower} >LowerCase </button>
+                </div>
+
+            </div>
+
+            <div className="container my-5">
+                <h4>Text Summary </h4>
+                <p>{inText.split(" ").length} Words and {inText.length} Charectors.</p>
             </div>
         </>
     )
+
+
 }
 
 export default TextArea
